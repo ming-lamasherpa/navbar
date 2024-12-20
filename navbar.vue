16a05719c691c97139ed85386adbdf006a5f3702
ming-lamasherpa <smingma767@gmail.com>
2024-12-20 21:31:44 +0545

    Add files via upload

---
+++ navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class ="pb-16">
+        <v-app-bar title="Online Learning website" color="black">
+          <v-btn to="/semester">Semester</v-btn>
+          <v-btn to="/viva">Viva questions</v-btn>
+          <v-btn to="/contact">contact now</v-btn>
+        </v-app-bar>
+    </div>
+</template>
+        
+<script>
+        
+        
+        
+</script>
+        
+<style>
+        
+        </style>
+
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
